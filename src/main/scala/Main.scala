5af5184627b9bedd00855009f640fd13ce286c4c
KazuyaMiyashita <kazuya.miyashita@septeni-original.co.jp>
2020-02-17 18:52:07 +0900

    ？

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,16 +1,30 @@
 import neko.server._
 
+import neko.http._
+
 object Main extends App {
 
   val routes = Routes(
-    GET     -> "/"           -> (_ => Response(OK, "Hello My Server!")),
-    POST    -> "/echo"       -> (req => Response(OK, req.body)),
-    GET     -> "/rooms"      -> neko.controller.ChatRooms.roomList,
-    POST    -> "/rooms"      -> neko.controller.ChatRooms.create,
-    GET     -> "/rooms/.*".r -> neko.controller.ChatRooms.messageList,
-    POST    -> "/rooms/.*".r -> neko.controller.ChatRooms.messageSend,
-    OPTIONS -> ".*".r        -> neko.controller.HandlePreflight.apply
+    GET  -> "/"     -> (_ => Response(OK, "Hello My Server!")),
+    POST -> "/echo" -> (req => Response(OK, req.body))
+    // GET     -> "/rooms"      -> neko.controller.ChatRooms.roomList,
+    // POST    -> "/rooms"      -> neko.controller.ChatRooms.create,
+    // GET     -> "/rooms/.*".r -> neko.controller.ChatRooms.messageList,
+    // POST    -> "/rooms/.*".r -> neko.controller.ChatRooms.messageSend,
+    // OPTIONS -> ".*".r        -> neko.controller.HandlePreflight.apply
   )
-  new Server(routes)
+
+  val requestHandler: ISocketRequestHandler = new HttpRequestHandler(routes)
+
+  val serverSocketHandler = new ServerSocketHandler(
+    requestHandler
+  )
+
+  new Thread(serverSocketHandler).start()
+
+  println("press enter to terminate")
+  io.StdIn.readLine()
+  println("closing..")
+  serverSocketHandler.close()
 
 }
--- src/main/scala/neko/server/Server.scala
+++ src/main/scala/neko/server/Server.scala
@@ -1,49 +1,102 @@
 package neko.server
 
 import java.net.ServerSocket
-import scala.io.BufferedSource
-import java.io.{BufferedWriter, OutputStreamWriter}
-import scala.concurrent.Future
-import scala.concurrent.ExecutionContext.Implicits.global
+import java.net.Socket
+import java.util.concurrent.ExecutorService
+import java.io.InputStream
+import java.io.OutputStream
+import java.util.concurrent.Executors
+import java.net.SocketException
 
-class Server(routes: Routes) {
+class ServerSocketHandler(
+    requestHandler: ISocketRequestHandler
+) extends Thread {
 
-  println("start >>>")
+  val serverSocket: ServerSocket             = new ServerSocket(2200)
+  val socketExecutorService: ExecutorService = Executors.newFixedThreadPool(32)
 
-  val server = new ServerSocket(2200)
-  Future {
+  var isActive                = true
+  var acceptionSocket: Socket = null
+  val socketManagaer          = new SocketManager
+
+  override def run(): Unit = {
     try {
-      while (true) {
-        val socket = server.accept()
-        val in     = new BufferedSource(socket.getInputStream())
-        val lines  = in.getLines.takeWhile(_.nonEmpty).toList
-        println("**request**")
-        lines.foreach(println)
-        println()
-        val header = RequestHeaderParser.parse(lines)
-        val body = header.contentLength match {
-          case None         => ""
-          case Some(length) => in.take(length).mkString
+      while (isActive) {
+        acceptionSocket = serverSocket.accept()
+        if (isActive) {
+          socketManagaer.register(acceptionSocket)
+          socketExecutorService.execute(new SocketHandler(acceptionSocket, requestHandler, socketManagaer))
         }
-        println(body)
-
-        val request  = Request(header, body)
-        val response = routes(request)
-
-        val out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))
-        out.write(response.view)
-        out.flush()
-
-        println("**response**")
-        println(response.view)
       }
     } catch {
-      case e: java.net.SocketException => println("[Socket closed]")
+      case _: SocketException => println("server socket closed")
+    } finally {
+      close()
     }
   }
 
-  io.StdIn.readLine()
-  server.close()
-  println("<<< end")
+  def close(): Unit = {
+    isActive = false
+    serverSocket.close()
+    socketExecutorService.shutdown()
+    if ((acceptionSocket ne null) && !acceptionSocket.isClosed()) {
+      acceptionSocket.close()
+    }
+    socketManagaer.closeAll()
+  }
 
+}
+
+class SocketHandler(
+    socket: Socket,
+    requestHandler: ISocketRequestHandler,
+    socketManager: SocketManager
+) extends Thread {
+
+  override def run(): Unit = {
+    val request = new ISocketRequest {
+      override def in: InputStream   = socket.getInputStream()
+      override def out: OutputStream = socket.getOutputStream()
+      override def close(): Unit     = onClose()
+    }
+    try {
+      requestHandler.handle(request)
+    } catch {
+      case _: SocketException => println("socket closed")
+    } finally {
+      onClose()
+    }
+  }
+
+  def onClose(): Unit = {
+    socket.close()
+    socketManager.release(socket)
+  }
+
+}
+
+class SocketManager {
+
+  import scala.collection.mutable
+  val sockets = mutable.Set.empty[Socket]
+
+  def register(s: Socket): Unit = sockets.add(s)
+  def release(s: Socket): Unit  = sockets.remove(s)
+
+  def closeAll(): Unit = {
+    sockets.foreach { s =>
+      if (!s.isClosed()) s.close()
+    }
+  }
+
+}
+
+trait ISocketRequest {
+  def in: InputStream
+  def out: OutputStream
+  def close(): Unit
+}
+
+trait ISocketRequestHandler {
+  def handle(request: ISocketRequest): Unit
 }
