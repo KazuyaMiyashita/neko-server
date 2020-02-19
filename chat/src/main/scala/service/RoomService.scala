e912bf3e2e47a7b3ce625a24a5703d79221fcd2b
KazuyaMiayshita <miy1924@gmail.com>
2020-02-20 04:23:24 +0900

    todo

---
+++ chat/src/main/scala/service/RoomService.scala
@@ -0,0 +1,22 @@
+package neko.chat.service
+
+import java.util.UUID
+import java.time.Clock
+import neko.core.jdbc.ConnectionIO
+import neko.chat.repository.{RoomRepository, UserRepository}
+import neko.chat.entity.{User, Room}
+
+class RoomService(
+  userRepository: UserRepository,
+  roomRepository: RoomRepository,
+  clock: Clock
+) {
+
+  def createRoom(user: User, roomName: String): ConnectionIO[Unit] = {
+    val room = Room(UUID.randomUUID(), roomName, clock.instant())
+    val e = for {
+      _ <- roomRepository.fetchByName(roomName)
+  }
+
+
+}
